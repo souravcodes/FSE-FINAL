63671030699cb3df913fc3328c666cf45ddbbe73
Admin <Admin@172.18.3.1>
2019-04-01 19:05:54 +0530

    second test

--- project-manager-back/src/main/java/com/fse/s1/projectmanager/TaskManagerApplication.java
+++ project-manager-back/src/main/java/com/fse/s1/projectmanager/TaskManagerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello 2");
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 
