ae8a2bc0771cffcac9424daece36bc1ff95b5510
Admin <Admin@172.18.3.1>
2019-04-01 19:07:59 +0530

    FSE final commit

--- project-manager-back/src/main/java/com/fse/s1/projectmanager/TaskManagerApplication.java
+++ project-manager-back/src/main/java/com/fse/s1/projectmanager/TaskManagerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello 2");
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 
